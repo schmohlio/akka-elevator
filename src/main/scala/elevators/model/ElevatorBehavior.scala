bc2c4f08a18aa4762820701fc335cf974ac4b150
Johannes Unterstein <unterstein@me.com>
2016-01-12 22:42:06 +0100

    ElevatorBehavior documentation

--- src/main/scala/elevators/model/ElevatorBehavior.scala
+++ src/main/scala/elevators/model/ElevatorBehavior.scala
@@ -7,6 +7,17 @@ sealed trait ElevatorBehavior {
   val currentFloor: Int
 }
 
+/**
+ * Representing that the according Elevator is idling.
+ *
+ * @param currentFloor the floor number where the Elevator idles
+ */
 case class Idle(currentFloor: Int) extends ElevatorBehavior
 
+/**
+ * Representing that the according Elevator is Moving.
+ *
+ * @param currentFloor the current floor number of the Elevator
+ * @param direction the traveling direction
+ */
 case class Moving(currentFloor: Int, direction: Direction) extends ElevatorBehavior
