531af59db2a49e4b76a023be373fb4ff8d23012b
Johannes Unterstein <unterstein@me.com>
2016-01-12 14:50:17 +0100

    moved spec and renamed it

---
+++ src/test/scala/elevators/ElevatorSpec.scala
@@ -0,0 +1,33 @@
+package elevators
+
+//import akka.actor.ActorSystem
+//import akka.testkit.{ImplicitSender, TestKit}
+//import com.example.{PingActor, PongActor}
+//import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+//
+//class ElevatorSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
+//  with WordSpecLike with Matchers with BeforeAndAfterAll {
+//
+//  def this() = this(ActorSystem("MySpec"))
+//
+//  override def afterAll {
+//    TestKit.shutdownActorSystem(system)
+//  }
+//
+//  "A Ping actor" must {
+//    "send back a ping on a pong" in {
+//      val pingActor = system.actorOf(PingActor.props)
+//      pingActor ! PongActor.PongMessage("pong")
+//      expectMsg(PingActor.PingMessage("ping"))
+//    }
+//  }
+//
+//  "A Pong actor" must {
+//    "send back a pong on a ping" in {
+//      val pongActor = system.actorOf(PongActor.props)
+//      pongActor ! PingActor.PingMessage("ping")
+//      expectMsg(PongActor.PongMessage("pong"))
+//    }
+//  }
+//
+//}
