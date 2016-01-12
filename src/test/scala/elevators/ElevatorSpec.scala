8091904c7e2399b3ce7a5a13d9133f5fb3dc2777
Johannes Unterstein <unterstein@me.com>
2016-01-12 16:52:26 +0100

    fixed bug

--- src/test/scala/elevators/ElevatorSpec.scala
+++ src/test/scala/elevators/ElevatorSpec.scala
@@ -28,7 +28,6 @@ class ElevatorSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitS
     "return a list of all elevator status" in {
       val elevatorActor = system.actorOf(ElevatorControlSystem.props(16))
       elevatorActor ! SystemStatusRequest
-      expectMsgClass(classOf[SystemStatusResponse])
       val answer = receiveOne(5.seconds).asInstanceOf[SystemStatusResponse]
       assert(answer.elevators.size == 16)
     }
