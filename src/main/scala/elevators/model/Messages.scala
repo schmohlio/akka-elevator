e41205c2762154a2f39d5dbe9f54198ba526f0d7
Johannes Unterstein <unterstein@me.com>
2016-01-12 15:42:09 +0100

    added first messages

---
+++ src/main/scala/elevators/model/Messages.scala
@@ -0,0 +1,27 @@
+package elevators.model
+
+/**
+ * @author Johannes Unterstein (unterstein@me.com)
+ */
+
+
+/**
+ * The request that can be sent to an ElevatorControlSystem to receive the system status
+ */
+case class SystemStatusRequest()
+
+/**
+ * The response for a SystemStatusRequest message
+ *
+ * @param elevators list representing the different elevator status
+ */
+case class SystemStatusResponse(elevators: List[ElevatorStatus])
+
+
+/**
+ * Representing the current status of an elevator
+ *
+ * @param id elevator id
+ * @param direction current traveling direction
+ */
+case class ElevatorStatus(id: Int, direction: Direction)
