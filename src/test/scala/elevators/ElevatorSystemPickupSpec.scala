f6389737aad7cdb0602dd188fa5a5fa59cb453d6
Johannes Unterstein <unterstein@me.com>
2016-01-12 22:27:54 +0100

    typo

--- src/test/scala/elevators/ElevatorSystemPickupSpec.scala
+++ src/test/scala/elevators/ElevatorSystemPickupSpec.scala
@@ -17,14 +17,14 @@ class ElevatorSystemPickupSpec(_system: ActorSystem) extends TestKit(_system) wi
 
   "A ElevatorControlSystem" must {
     "react to PickupRequest" in {
-      val elevatorControlSysstemActor = system.actorOf(ElevatorControlSystem.props(1))
-      elevatorControlSysstemActor ! PickupRequest(Passenger(10, 0))
+      val elevatorControlSystemActor = system.actorOf(ElevatorControlSystem.props(1))
+      elevatorControlSystemActor ! PickupRequest(Passenger(10, 0))
       Thread.sleep(1000) // wait a second until PickupRequest is scheduled
       (0 to 20).foreach {
         i =>
-          elevatorControlSysstemActor ! Tick
+          elevatorControlSystemActor ! Tick
       }
-      elevatorControlSysstemActor ! SystemStatusRequest
+      elevatorControlSystemActor ! SystemStatusRequest
       val answer = receiveOne(5.seconds).asInstanceOf[SystemStatusResponse]
       assert(answer.elevators.size == 1)
       assert(answer.elevators(0).direction.isInstanceOf[Idle])
